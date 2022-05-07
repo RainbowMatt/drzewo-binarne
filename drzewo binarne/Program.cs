70da15c91f6613dc46ab3cfcda3176a347da3c23
Mateusz Witulski <Mateusz.Witulski@microsoft.wsei.edu.pl>
2022-05-07 14:25:21 +0200

    zadanie 3 nieskonczone do przerobienia

--- drzewo binarne/Program.cs
+++ drzewo binarne/Program.cs
@@ -15,7 +15,7 @@ class Program
                     new BinTreeNode<char>('G')
                  );
         var t = new BinTreeNode<char>('A', tB, tC);
-        Print(t);
+        Console.WriteLine(NoOfNodes(t));
     }
     public static BinTreeNode<char> CreateTreeOfChars()
     {
@@ -32,14 +32,24 @@ class Program
         return tree;
     }
 
-    public static void Print<T>(BinTreeNode<T> p, int level = 0)
+    public static void PrintTree<T>(BinTreeNode<T> p, int level = 0)
     {
         if (p == null) return;
-        Print(p.Left, level + 1);
         Console.WriteLine("".PadLeft(level, '.') + p.Value);
-        Print(p.Right, level + 1);
+        PrintTree(p.Left, level + 1);
+        PrintTree(p.Right, level + 1);
     }
-
+    public static int NoOfNodes<T>(BinTreeNode<T> tree)
+    {
+        int i=0;
+        while(tree != null)
+        {
+            i++;
+            tree = tree.Left;
+            if (tree == null) tree = tree.Right;
+        }
+        return i;
+    }
 }
 
 public class BinTreeNode<T>
